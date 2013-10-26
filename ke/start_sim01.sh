95555a34ad1d50bf56c1940babeaf78f775eedcc
zunlin1234 <zunlin1234@gmail.com>
2013-10-26 01:11:03 -0700

    start sim01 with logconfig inimaster inifile inidirectory

--- ke/start_sim01.sh
+++ ke/start_sim01.sh
@@ -1,3 +1,9 @@
+# http://opensimulator.org/wiki/OpenSim.exe_Command_Line_Options
+# logconfig (default)OpenSim.exe.config
+# background (default)false
+# inimaster  (default)OpenSimDefaults.ini
+# inifile    (default)OpenSim.ini
+# inidirectory ./grid/sim01/
 export MONO_THREADS_PER_CPU=125
 cd ~/ke/opensim/opensim75/bin
-mono ./OpenSim.exe -logconfig=./grid/sim01/OpenSim.exe.config -inidirectory=./grid/sim01/ 
+mono ./OpenSim.exe -logconfig=./grid/sim01/OpenSim.exe.config -inimaster=OpenSimDefaults.ini -inifile=OpenSim.ini -inidirectory=./grid/sim01/ 
