a4a9075db9c05e65c372b90406d9e4c0cff215f1
zunlin1234 <zunlin1234@gmail.com>
2013-10-26 00:23:44 -0700

    add grid folder to contain multiple sim instances(eg sim01)

--- ke/start_robust.asset.sh
+++ ke/start_robust.asset.sh
@@ -1,1 +1,2 @@
+cd ~/ke/opensim/opensim75/bin
 mono ./Robust.exe -inifile=./Robust.Assets.ini -logfile= ./Robust.Assets.log
---
+++ ke/start_robust.sh
@@ -0,0 +1,2 @@
+cd ~/ke/opensim/opensim75/bin
+mono ./Robust.exe -inifile=./Robust.ini -logfile=./Robust.log
