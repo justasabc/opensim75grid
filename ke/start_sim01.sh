a783a9d3dc2db071acd2a8146408685f15f8f50c
zunlin1234 <zunlin1234@gmail.com>
2013-10-26 00:59:32 -0700

    sim01 log to itself log file defined in opensim.exe.config

--- ke/start_sim01.sh
+++ ke/start_sim01.sh
@@ -1,3 +1,3 @@
 export MONO_THREADS_PER_CPU=125
 cd ~/ke/opensim/opensim75/bin
-mono ./OpenSim.exe -inidirectory=./grid/sim01/ -logfile=./grid/sim01/OpenSim.log
+mono ./OpenSim.exe -logconfig=./grid/sim01/OpenSim.exe.config -inidirectory=./grid/sim01/ 
