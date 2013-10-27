f5a2f86a8e5a7f7e85c323f1ab6b4dd62957d0e4
zunlin1234 <zunlin1234@gmail.com>
2013-10-26 23:41:45 -0700

    add shell environment variable support for xxx.ini xxx.exe.config

--- bin/ke/start_sim01.sh
+++ bin/ke/start_sim01.sh
@@ -4,6 +4,14 @@
 # inimaster  (default)OpenSimDefaults.ini
 # inifile    (default)OpenSim.ini
 # inidirectory ./grid/sim01/
-./env.sh
-cd ~/ke/opensim/opensim75/bin
-mono ./OpenSim.exe -logconfig=./grid/instances/sim01/sim01.exe.config -inimaster=OpenSimDefaults.ini -inifile=OpenSim.ini -inidirectory=./grid/instances/sim01/ 
+./common_env.sh
+bin=`dirname "$0"`
+bin=`cd "../$bin"; pwd`
+cd $bin
+
+export SIM_NAME="sim01"
+export SIM_DIR="./grid/instances/$SIM_NAME"
+export SIM_LOG_CONFIG="$SIM_DIR/$SIM_NAME.exe.config"
+export SIM_LOG_FILE="$SIM_DIR/$SIM_NAME.log" 
+# SIM_LOG_FILE will be used in xxx.exe.config
+mono ./OpenSim.exe -logconfig=$SIM_LOG_CONFIG -inimaster=OpenSimDefaults.ini -inifile=OpenSim.ini -inidirectory=$SIM_DIR
